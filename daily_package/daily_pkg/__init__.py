44d2fdab4496e5871eeea3431f3b4cd51cbef145
CharGoon26 <chargonzales26@gmail.com>
2026-03-06 03:31:15 +0800

    Delete daily_package directory

--- daily_package/daily_pkg/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = "daily_pkg.apps.DailyPkgConfig"
--- daily_package/daily_pkg/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DailyPkgConfig(AppConfig):
-    name = "daily_pkg"
-    dpy_package = "daily_pkg.daily_ext"
